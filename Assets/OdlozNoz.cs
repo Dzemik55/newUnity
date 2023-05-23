bc440e1786f049b5379dea1bc8a58c04a1d29b15
Tomasz Rycko <tomasz.rycko@gmail.com>
2023-05-23 17:34:19 +0200

    Podnoszenie noza dziala jak trzeba (na 99%)

---
+++ Assets/OdlozNoz.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+
+public class OdlozNoz : MonoBehaviour
+{
+    public Camera playerCamera;
+    public bool nozPodniesiony = false;
+    public bool nozOdlozony = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!nozPodniesiony && nozOdlozony)
+        {
+            PodniesNoz();
+        }
+        else
+        {
+            OdlozNozyk();
+        }
+
+
+
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            if (PickUpKnife.nozPodniesiony && !PickUpKnife.nozOdlozony)
+            {
+                Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.gameObject.name != "knife")
+                    {
+                        Vector3 itemPosition = hit.point;
+                        Transform knifeTransform = playerCamera.transform.Find("knife");
+
+                        if (knifeTransform != null)
+                        {
+                            GameObject knife = knifeTransform.gameObject;
+                            knife.transform.parent = null;
+                            knife.transform.position = itemPosition;
+                        }
+                    }
+                }
+
+                Debug.Log(PickUpKnife.nozOdlozony);
+            }
+        }*/
+
+    }
+
+    public void OdlozNozyk()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 itemPosition = hit.point + new Vector3(0f, 0.5f, 0f);
+                Transform knifeTransform = playerCamera.transform.Find("knife");
+
+                if (knifeTransform != null)
+                {
+                    GameObject knife = knifeTransform.gameObject;
+                    knife.transform.parent = null;
+                    knife.transform.position = itemPosition;
+                    knife.GetComponent<Rigidbody>().useGravity = true;
+                    knife.GetComponent<Rigidbody>().isKinematic = false;
+                    knife.GetComponent<BoxCollider>().enabled = true;
+                    knife.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                    knife.transform.localScale = new Vector3(0.0300099328f, 0.0300102904f, 0.024555305f);
+                    this.nozPodniesiony = false;
+                    this.nozOdlozony = true;
+                }
+            }
+        }
+    }
+
+    public void PodniesNoz()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name == "knife")
+                {
+                    Debug.Log("Nó¿ trafiony");
+                    Transform clickedObject = hit.transform;
+                    Transform parentObject = GameObject.Find("First Person Camera").transform; // Obiekt, który ma zostaæ nowym rodzicem
+                    clickedObject.SetParent(parentObject);
+                    Vector3 newPosition = new Vector3(0.583999991f, -0.321999997f, 0.819999993f);
+                    Quaternion newRotation = new Quaternion(-0.867192209f, 0.0977871269f, -0.0817015246f, 0.481394112f);
+                    clickedObject.GetComponent<Rigidbody>().useGravity = false;
+                    clickedObject.GetComponent<Rigidbody>().isKinematic = true;
+                    clickedObject.GetComponent<BoxCollider>().enabled = false;
+                    clickedObject.transform.localRotation = newRotation;
+                    clickedObject.transform.localPosition = newPosition;
+                    clickedObject.transform.localScale = new Vector3(0.0300099328f, 0.0300102904f, 0.024555305f);
+
+
+                    clickedObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePosition;
+
+                    this.nozPodniesiony = true;
+                    this.nozOdlozony = false;
+
+                }
+            }
+        }
+    }
+}
+
--- Assets/PickUpKnife.cs
+++ Assets/PickUpKnife.cs
@@ -3,37 +3,43 @@ using UnityEngine;
 public class PickUpKnife : MonoBehaviour
 {
     public Camera playerCamera;
-    public GameObject yourItemPrefab;
-    public bool nozPodniesiony = false;
-    public bool nozOdlozony = true;
+    public static bool nozPodniesiony = false;
+    public static bool nozOdlozony = true;
     void Start()
     {
 
     }
 
+
+
     // Update is called once per frame
     void OnMouseDown()
     {
-        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
+        if (!nozPodniesiony && nozOdlozony)
         {
-            Debug.Log("Nó¿ trafiony");
-            Transform clickedObject = hit.transform;
-            Transform parentObject = GameObject.Find("First Person Camera").transform; // Obiekt, który ma zostaæ nowym rodzicem
-            //clickedObject.SetParent(parentObject);
-            Vector3 newPosition = new Vector3(0.352999985f, -0.181999996f, 0.504000008f);
-            Vector3 newRotation = new Vector3(273.758057f, 180f, 180f);
-            clickedObject.transform.position = newPosition;
-            /*clickedObject.transform.rotation = Quaternion.*/
-            clickedObject.GetComponent<Rigidbody>().useGravity = false;
-            clickedObject.GetComponent<Rigidbody>().isKinematic = true;
+            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-            clickedObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePosition;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Nó¿ trafiony");
+                Transform clickedObject = hit.transform;
+                Transform parentObject = GameObject.Find("First Person Camera").transform; // Obiekt, który ma zostaæ nowym rodzicem
+                clickedObject.SetParent(parentObject);
+                Vector3 newPosition = new Vector3(0.583999991f, -0.321999997f, 0.819999993f);
+                Quaternion newRotation = new Quaternion(-0.867192209f, 0.0977871269f, -0.0817015246f, 0.481394112f);
+                clickedObject.GetComponent<Rigidbody>().useGravity = false;
+                clickedObject.GetComponent<Rigidbody>().isKinematic = true;
+                clickedObject.GetComponent<MeshCollider>().enabled = false;
+                clickedObject.transform.localRotation = newRotation;
+                clickedObject.transform.localPosition = newPosition;
 
-            nozPodniesiony = true;
-            nozOdlozony = false;
+
+                clickedObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePosition;
+
+                nozPodniesiony = true;
+                nozOdlozony = false;
+            }
         }
     }
 
